2c124f0fe2af425fca1f465bbf958d55e21488d9
ajayjoseph01 <ajaythankachan01@gmail.com>
2022-03-18 01:40:39 +0530

    ajay

--- Training/views.py
+++ Training/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
 
 # Create your views here.
+def admin_Dashboard(request):
+    return render(request,'admin/admin_Dashboard.html')
+
+def admin_categories(request):
+    return render(request,'admin/admin_categories.html') 
+
+def admin_emp_categories(request):
+    return render(request,'admin/admin_emp_categories.html')  
+
+def admin_courses(request):
+    return render(request,'admin/admin_courses.html')
+
+def admin_emp_course_list(request):
+    return render(request,'admin/admin_emp_course_list.html')
+
+def admin_emp_course_details(request):
+    return render(request,'admin/admin_emp_course_details.html')
+
+def admin_emp_profile(request):
+    return render(request,'admin/admin_emp_profile.html')
+
+def admin_emp_attendance(request):
+    return render(request,'admin/admin_emp_attendance.html')
+
+def admin_emp_attendance_show(request):
+    return render(request,'admin/admin_emp_attendance_show.html')
+
+def admin_task(request):
+    return render(request,'admin/admin_task.html')
+
+def admin_givetask(request):
+    return render(request,'admin/admin_givetask.html')
+
+def admin_current_task(request):
+    return render(request,'admin/admin_current_task.html')
+
+def admin_previous_task(request):
+    return render(request,'admin/admin_previous_task.html')
+
+def admin_registration_details(request):
+    return render(request,'admin/admin_registration_details.html')  
+
+def admin_attendance(request):
+    return render(request,'admin/admin_attendance.html') 
+
+def admin_attendance_show(request):
+    return render(request,'admin/admin_attendance_show.html')
+
+def admin_reported_issues(request):
+    return render(request,'admin/admin_reported_issues.html') 
+
+def admin_emp_reported_detail(request):
+    return render(request,'admin/admin_emp_reported_detail.html')
+
+def admin_emp_reported_issue_show(request):
+    return render(request,'admin/admin_emp_reported_issue_show.html')
+
+def admin_manager_reported_detail(request):
+    return render(request,'admin/admin_manager_reported_detail.html')
+
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Training import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('admin_Dashboard/', views.admin_Dashboard, name='admin_Dashboard'),
+    path('admin_categories/', views.admin_categories, name='admin_categories'),
+    path('admin_emp_categories/', views.admin_emp_categories, name='admin_emp_categories'),
+    path('admin_courses/', views.admin_courses, name='admin_courses'),
+    path('admin_emp_course_list/', views.admin_emp_course_list,name='admin_emp_course_list'),
+    path('admin_emp_course_details/', views.admin_emp_course_details,name='admin_emp_course_details'),
+    path('admin_emp_profile/', views.admin_emp_profile, name='admin_emp_profile'),
+    path('admin_emp_attendance/', views.admin_emp_attendance,name='admin_emp_attendance'),
+    path('admin_emp_attendance_show/', views.admin_emp_attendance_show,name='admin_emp_attendance_show'),
+    path('admin_task/', views.admin_task, name='admin_task'),
+    path('admin_givetask/', views.admin_givetask, name='admin_givetask'),
+    path('admin_current_task/', views.admin_current_task,name='admin_current_task'),
+    path('admin_previous_task/', views.admin_previous_task,name='admin_previous_task'),
+    path('admin_registration_details/', views.admin_registration_details,name='admin_registration_details'), 
+    path('admin_attendance/', views.admin_attendance, name='admin_attendance'),
+    path('admin_attendance_show/', views.admin_attendance_show,name='admin_attendance_show'),
+    path('admin_reported_issues/', views.admin_reported_issues,name='admin_reported_issues'),
+    path('admin_emp_reported_detail/', views.admin_emp_reported_detail,name='admin_emp_reported_detail'),
+    path('admin_emp_reported_issue_show/', views.admin_emp_reported_issue_show,name='admin_emp_reported_issue_show'),
+    path('admin_manager_reported_detail/', views.admin_manager_reported_detail,name='admin_manager_reported_detail'),
+    
+
 ]
